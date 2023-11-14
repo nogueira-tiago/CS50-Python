6d3c22d7c62374452ae1c0b57da9f3b9aca9a7ae
nogueira-tiago <123997903+nogueira-tiago@users.noreply.github.com>
2023-11-14 18:11:15 -0400

    terminou
    fiz um somador e aprendi a definir funções

---
+++ aula001/ola2.0.py
@@ -0,0 +1,5 @@
+def ola(para="mundo"):
+    print("ola,",para)
+
+nome = input("Qual o seu nome ?").strip().title()
+ola(nome)    
---
+++ aula001/somador.py
@@ -0,0 +1,5 @@
+x = float(input("Qual o valor de x ?"))
+y = float(input("Qual o valor de y ?"))
+z = x+y
+
+print(f"{z:,}")
