98ee01d2522dd5e9886effbe563e698cb02bef8c
nogueira-tiago <123997903+nogueira-tiago@users.noreply.github.com>
2023-12-18 20:48:01 -0400

    rapais que treta

---
+++ problem_set_6/scourgify/scourgify.py
@@ -0,0 +1,54 @@
+import sys
+import csv
+import os
+
+def convert_csv(input_file, output_file):
+    try:
+        with open(input_file, "r") as input_csv, open(output_file, "w", newline="") as output_csv:
+            reader =csv.reader(input_csv)
+            writer = csv.writer(output_csv)
+        
+            for row in reader:
+                if len(row) !=2:
+                    print("Usage: python scourgify.py <filename.csv> <outputfile.csv>")
+                    sys.exit(1)
+
+                full_name = row[0].strip('"')
+                house = row[1]
+
+                name_parts = full_name.split(',')
+                if len(name_parts) != 2:
+                    print(f"Error: Invalid name format in the input CSV: {row[0]}")
+
+                
+                first_name, last_name = name_parts
+
+                writer.writerow([first_name.strip(),last_name.strip(),house.strip()])
+    
+    except FileNotFoundError:
+        print("Usage: python program.py <input_file.csv> <output_file.csv>")
+        sys.exit(1)
+
+def find_file_path(directory, file_name):
+    for root, dirs, files in os.walk(directory):
+        if  file_name in files:
+            return os.path.join(root, file_name)
+    return None
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python program.py <input_file.csv> <output_file.csv>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    current_directory = os.getcwd()
+    file_path = find_file_path(current_directory, input_file)
+
+    if file_path is None:
+         print(f"Error: File '{input_file}' not found")
+
+    convert_csv(input_file, output_file)
+
+if __name__ == "__main__":
+    main()
